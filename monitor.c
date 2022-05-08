fcea2fcd7145b256365b74e1470985066a6b15f8
Ivchenko Nikolay <mrsheldie@mail.ru>
2022-05-08 20:04:44 +0300

    to norm

--- monitor.c
+++ monitor.c
@@ -6,7 +6,7 @@
 /*   By: gannemar <gannemar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 19:19:06 by gannemar          #+#    #+#             */
-/*   Updated: 2022/05/08 19:10:45 by gannemar         ###   ########.fr       */
+/*   Updated: 2022/05/08 19:50:19 by gannemar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,38 @@
 
 #include "philosophers.h"
 
+static int	check_all_filo(t_prime *prime)
+{
+	ssize_t		i;
+	long int	curr_time;
+
+	i = -1;
+	while (++i < prime->philo_nb)
+	{
+		pthread_mutex_lock(&prime->last_eating_time_mutexes[i]);
+		pthread_mutex_lock(&prime->finish_mutex);
+		curr_time = get_curr_time();
+		if (curr_time - prime->philos[i].last_eating_time
+			> prime->philos[i].time_to_die)
+		{
+			if (!prime->finish)
+			{
+				prime->finish = true;
+				printf("%ld %ld died\n", curr_time - prime->start_time, i);
+				pthread_mutex_unlock(&prime->last_eating_time_mutexes[i]);
+				pthread_mutex_unlock(&prime->finish_mutex);
+				return (EXIT_THREAD);
+			}
+		}
+		pthread_mutex_unlock(&prime->finish_mutex);
+		pthread_mutex_unlock(&prime->last_eating_time_mutexes[i]);
+	}
+	return (CONTINUE_THREAD);
+}
+
 static void	*monitor(void *_arg)
 {
 	t_prime		*arg;
-	ssize_t		i;
-	long int	curr_time;
 
 	arg = (t_prime *)_arg;
 	while (true)
@@ -30,29 +57,9 @@ static void	*monitor(void *_arg)
 			return (NULL);
 		}
 		pthread_mutex_unlock(&arg->finish_mutex);
-		i = -1;
-		while (++i < arg->philo_nb)
-		{
-			pthread_mutex_lock(&arg->last_eating_time_mutexes[i]);
-			pthread_mutex_lock(&arg->finish_mutex);
-			curr_time = get_curr_time();
-			if (curr_time - arg->philos[i].last_eating_time
-				> arg->philos[i].time_to_die)
-			{
-				if (!arg->finish)
-				{
-					arg->finish = true;
-					printf("%ld %ld died\n", curr_time - arg->start_time, i);
-					pthread_mutex_unlock(&arg->last_eating_time_mutexes[i]);
-					pthread_mutex_unlock(&arg->finish_mutex);
-					return (NULL);
-				}
-			}
-			pthread_mutex_unlock(&arg->finish_mutex);
-			pthread_mutex_unlock(&arg->last_eating_time_mutexes[i]);
-		}
+		if (check_all_filo(arg))
+			return (NULL);
 	}
-	return (NULL);
 }
 
 int	start_monitor(t_prime *prime)
